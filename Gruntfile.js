969f89b066a5c9e57e72c17254b150ed8f0a12db
Andrew Savory <asavory@apache.org>
2013-09-21 13:44:07 +0000

    DMAP-28 - make grunt test work again * Update karma.conf.js to latest format. * Update list of npm modules. * Switch test to use firefox as phantomjs is hanging. * Tell svn to ignore target and node_modules
    git-svn-id: https://svn.apache.org/repos/asf/incubator/devicemap/trunk/browsermap/trunk@1525237 13f79535-47bb-0310-9956-ffa450edef68

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
             continuous: {
                 configFile: 'src/test/karma.conf.js',
                 singleRun: true,
-                browsers: ['PhantomJS']
+                browsers: ['Firefox']
             }
         },
         jshint: {
--- src/test/karma.conf.js
+++ src/test/karma.conf.js
@@ -18,17 +18,13 @@
  */
 
 // Testacular configuration
-// Generated on Fri Jan 25 2013 15:43:08 GMT+0200 (EET)
-
-
-// base path, that will be used to resolve files and exclude
-basePath = '../../';
-
-
-// list of files / patterns to load in the browser
-files = [
-    QUNIT,
-    QUNIT_ADAPTER,
+module.exports = function(config) {
+  config.set({
+    // base path, that will be used to resolve files and exclude
+    basePath: '../../',
+    frameworks: ['qunit'],
+    // list of files / patterns to load in the browser
+    files: [
     'src/main/js/bmaputil.js',
     'src/main/js/bmap.js',
     'src/main/lib/matchMedia/matchMedia.js',
@@ -36,62 +32,51 @@ files = [
     'src/main/js/probes.js',
     'src/main/js/devicegroups.js',
     'src/test/js/*.js'
-];
+    ],
+    exclude: [ ],
+    reporters: ['progress', 'junit'],
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit'
+    junitReporter: {
+        outputFile: 'target/test-results.xml'
+    },
+    // web server port
+    port: 8080,
+    // cli runner port
+    runnerPort: 9100,
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['Chrome', 'ChromeCanary', 'Firefox'],
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 30000,
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    logLevel: config.LOG_INFO,
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false,
+    // report which specs are slower than 500ms
+    // CLI --report-slower-than 500
+    reportSlowerThan: 500,
+    //...
+  });
+};
 
 
-// list of files to exclude
-exclude = [
-
-];
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress', 'junit'];
-junitReporter = {
-    outputFile: 'target/test-results.xml'
-}
-
-// web server port
-port = 8080;
 
 
-// cli runner port
-runnerPort = 9100;
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
-
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
-
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome', 'ChromeCanary', 'Firefox'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 30000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
-
-// report which specs are slower than 500ms
-// CLI --report-slower-than 500
-reportSlowerThan = 500;
